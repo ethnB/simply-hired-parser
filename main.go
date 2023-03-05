3312e219d03c2883ca1af78a3bbd65e28de9ad9e
Ethan Beever <ethanbeever@gmail.com>
2023-03-05 01:08:50 +0000

    cleanup, add better logs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -21,23 +22,31 @@ func check(err error) {
 	}
 }
 
-var baseUrl string = "https://www.simplyhired.co.uk/"
-var keyPattern, patternErr = regexp.Compile(`/job/(.*)\?`)
+const baseUrl string = "https://www.simplyhired.co.uk/"
+const outputDir string = "./output/"
 
-func worker(id int, jobs <-chan scrapeJob, ack chan<- bool) {
-	log.Printf("started worker %d", id)
+// max site pages to scrape
+const pageLimit int = 2
+
+// worker routine count
+const workers int = 5
+
+// required to parse out the job key required for the details API
+var keyPattern, _ = regexp.Compile(`/job/(.*)\?`)
+
+func worker(wid int, jobs <-chan scrapeJob, complete chan<- bool) {
+	log.Printf("worker=%d started", wid)
 
 	for scrapeJob := range jobs {
 		url := fmt.Sprintf(
 			"%ssearch?q=%s&l=%s&pn=%d&from=pagination",
 			baseUrl,
-			url.QueryEscape(scrapeJob.query),
+			url.QueryEscape(scrapeJob.searchQuery),
 			url.QueryEscape(scrapeJob.location),
 			scrapeJob.page,
 		)
 
-		log.Printf("retrieving page %s\n", url)
-
+		log.Printf("worker=%d retrieving page %s", wid, url)
 		resp, err := http.Get(url)
 		check(err)
 
@@ -45,25 +54,38 @@ func worker(id int, jobs <-chan scrapeJob, ack chan<- bool) {
 		check(err)
 		resp.Body.Close()
 
-		out := doc.Find(".SerpJob-link").Map(func(i int, sel *goquery.Selection) string {
+		// parse the job cards in the HTML into the data we need for API queries
+		jobLinks := doc.Find(".SerpJob-link").Map(func(i int, sel *goquery.Selection) string {
 			ref, _ := sel.Attr("data-mdref")
-			title := sel.Text()
-			parsed := map[string]string{"ref": ref, "text": title}
+			jobTitle := sel.Text()
+			parsed := map[string]string{
+				"ref":   ref,
+				"title": jobTitle,
+			}
 			bytes, err := json.Marshal(parsed)
 			check(err)
-
 			return string(bytes)
 		})
 
-		log.Printf("worker=%d found %d jobs", id, len(out))
+		jobCount := len(jobLinks)
+		log.Printf("worker=%d found %d jobs", wid, jobCount)
 
-		for _, job := range out {
+		parentDirectory := fmt.Sprintf("%spage_%d/", outputDir, scrapeJob.page)
+		log.Printf("worker=%d writing to %s", wid, parentDirectory)
+
+		for _, job := range jobLinks {
 			var discovered map[string]string
 			json.Unmarshal([]byte(job), &discovered)
 
-			key := keyPattern.FindStringSubmatch(discovered["ref"])[1]
+			ref := discovered["ref"]
+			title := discovered["title"]
 
-			resp, err := http.Get("https://www.simplyhired.co.uk/api/job?key=" + key)
+			key := keyPattern.FindStringSubmatch(ref)[1]
+
+			jobApiUrl := fmt.Sprintf("https://www.simplyhired.co.uk/api/job?key=%s", key)
+
+			log.Printf("worker=%d page=%d pulling job details for '%s'", wid, scrapeJob.page, title)
+			resp, err := http.Get(jobApiUrl)
 			check(err)
 
 			read, err := io.ReadAll(resp.Body)
@@ -73,15 +95,14 @@ func worker(id int, jobs <-chan scrapeJob, ack chan<- bool) {
 			var jobResponse jobApiResponse
 			json.Unmarshal(read, &jobResponse)
 
+			// sanitize the filename somewhat
 			filename := strings.ReplaceAll(jobResponse.Job.Title, " ", "_")
 			filename = strings.ReplaceAll(filename, "/", "_")
 
-			parentDirectory := fmt.Sprintf("./output/page_%d/", scrapeJob.page)
-
-			// ensure the page directory exists
+			// ensure the page-level directory exists
 			os.MkdirAll(parentDirectory, os.ModePerm)
 
-			outputFile := fmt.Sprintf("%sjob_%s", parentDirectory, filename)
+			outputFile := fmt.Sprintf("%s%s", parentDirectory, filename)
 
 			var formatted bytes.Buffer
 			json.Indent(&formatted, read, "", "\t")
@@ -90,27 +111,31 @@ func worker(id int, jobs <-chan scrapeJob, ack chan<- bool) {
 			check(writeErr)
 		}
 
-		ack <- true
+		log.Printf("worker=%d written %d jobs to %s", wid, jobCount, parentDirectory)
+
+		complete <- true
 	}
 }
 
 func main() {
-	check(patternErr)
+	// location e.g. 'Greater Manchester'
+	location := flag.String("location", "", "location to search for jobs e.g. 'Greater Manchester'")
+	// search query string e.g. 'Software Engineer'
+	searchQuery := flag.String("query", "", "job to search for e.g. 'Software Engineer'")
 
-	args := os.Args[1:]
+	flag.Parse()
 
-	query := args[0]
-	loc := args[1]
+	if *searchQuery == "" {
+		log.Panic("query is a required flag (-query \"Software Engineer\")")
+	}
 
-	// max pages
-	const pageLimit = 5
-
-	// worker routine count
-	const workers = 5
+	if *location == "" {
+		log.Panic("location is a required flag (-location \"Greater Manchester\")")
+	}
 
 	// ensure the output location exists and is empty
-	os.RemoveAll("./output/")
-	os.MkdirAll("./output/", os.ModePerm)
+	os.RemoveAll(outputDir)
+	os.MkdirAll(outputDir, os.ModePerm)
 
 	jobs := make(chan scrapeJob, pageLimit)
 	results := make(chan bool, pageLimit)
@@ -120,23 +145,23 @@ func main() {
 
 	for i := 1; i <= pageLimit; i++ {
 		jobs <- scrapeJob{
-			location: loc,
-			query:    query,
-			page:     i,
+			location:    *location,
+			searchQuery: *searchQuery,
+			page:        i,
 		}
 	}
 	close(jobs)
 
 	for a := 1; a <= pageLimit; a++ {
-		// wait for each job to write its output files
+		// wait for all jobs to complete
 		<-results
 	}
 }
 
 type scrapeJob struct {
-	location string
-	query    string
-	page     int
+	location    string
+	searchQuery string
+	page        int
 }
 
 type jobApiResponse struct {
